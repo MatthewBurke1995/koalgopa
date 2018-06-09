d2a54285e4397cd795236733b931328e75ad5d9c
Your Name <mperoburke@gmail.com>
2018-06-09 22:22:52 +1000

    change make account wording and add image logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', '코알고파') }}
+                    <img src="img/koalgopa_logo_small.jpg" style="height:50px">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
